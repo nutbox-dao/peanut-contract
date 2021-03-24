45b187449496f8aaa0f4bf56b7b66a3917aed3ca
wangxi <wangxi8206@sina.com>
2021-03-24 11:12:44 +0800

    fix issue

--- contracts/TsteemPool.sol
+++ contracts/TsteemPool.sol
@@ -5,25 +5,32 @@ import "./Ownable.sol";
 
 interface ERC20Token {
     function transfer(address _to, uint256 _value) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
     function balanceOf(address account) external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
 }
 
 interface PeanutsPool {
     function withdrawPeanuts() external;
+
     function getPendingPeanuts() external view returns (uint256);
 }
 
 contract TsteemPool is Ownable {
     using SafeMath for uint256;
-
     struct Delegator {
-        uint256 tsteemAmount;          // deposted tsteem
-        uint256 availablePeanuts;   // minted peanuts for user
-        uint256 debtRewards;        // rewards debt 
-        bool hasDeposited;          // set true when first time deposit
+        uint256 tsteemAmount; // deposted tsteem
+        uint256 availablePeanuts; // minted peanuts for user
+        uint256 debtRewards; // rewards debt
+        bool hasDeposited; // set true when first time deposit
     }
-
     mapping(address => Delegator) public delegators;
     address[] public delegatorsList;
     uint256 public shareAcc;
@@ -40,11 +47,18 @@ contract TsteemPool is Ownable {
     event WithdrawPeanuts(address delegator, uint256 amount);
 
     modifier onlyDelegator() {
-        require(delegators[msg.sender].hasDeposited, "Account is not a delegator");
+        require(
+            delegators[msg.sender].hasDeposited,
+            "Account is not a delegator"
+        );
         _;
     }
 
-    constructor (address _punts, address _pnutPool, address _tsteem) public {
+    constructor(
+        address _punts,
+        address _pnutPool,
+        address _tsteem
+    ) public {
         Pnuts = ERC20Token(_punts);
         Tsteem = ERC20Token(_tsteem);
         PnutPool = PeanutsPool(_pnutPool);
@@ -55,17 +69,12 @@ contract TsteemPool is Ownable {
     }
 
     // should do this before game start
-    function setDevAddress(address dev) 
-        public
-        onlyOwner
-    {
+    function setDevAddress(address dev) public onlyOwner {
         require(dev != address(0), "Invalid dev address");
         devAddress = dev;
     }
 
-    function deposit(uint256 _amount)
-        public
-    {
+    function deposit(uint256 _amount) public {
         if (_amount == 0) return;
 
         // totalDepositedTSTEEM == 0 means there is not delegator exist. When first delegator come,
@@ -78,12 +87,15 @@ contract TsteemPool is Ownable {
         }
 
         uint256 tsteemBalance = Tsteem.balanceOf(msg.sender);
-        require(_amount <= tsteemBalance, "ERC20: transfer amount exceeds balance");
+        require(
+            _amount <= tsteemBalance,
+            "ERC20: transfer amount exceeds balance"
+        );
 
         Tsteem.transferFrom(msg.sender, address(this), _amount);
 
         // Add to delegator list if account hasn't deposited before
-        if(!delegators[msg.sender].hasDeposited) {
+        if (!delegators[msg.sender].hasDeposited) {
             delegators[msg.sender].hasDeposited = true;
             delegators[msg.sender].availablePeanuts = 0;
             delegators[msg.sender].tsteemAmount = 0;
@@ -94,81 +106,112 @@ contract TsteemPool is Ownable {
         _updateRewardInfo();
 
         if (delegators[msg.sender].tsteemAmount > 0) {
-            uint256 pending = delegators[msg.sender].tsteemAmount.mul(shareAcc).div(1e12).sub(delegators[msg.sender].debtRewards);
-            if(pending > 0) {
-                delegators[msg.sender].availablePeanuts = delegators[msg.sender].availablePeanuts.add(pending);
+            uint256 pending =
+                delegators[msg.sender].tsteemAmount.mul(shareAcc).div(1e12).sub(
+                    delegators[msg.sender].debtRewards
+                );
+            if (pending > 0) {
+                delegators[msg.sender].availablePeanuts = delegators[msg.sender]
+                    .availablePeanuts
+                    .add(pending);
             }
         }
 
-        delegators[msg.sender].tsteemAmount = delegators[msg.sender].tsteemAmount.add(_amount);
+        delegators[msg.sender].tsteemAmount = delegators[msg.sender]
+            .tsteemAmount
+            .add(_amount);
         totalDepositedTSTEEM = totalDepositedTSTEEM.add(_amount);
 
-        delegators[msg.sender].debtRewards = delegators[msg.sender].tsteemAmount.mul(shareAcc).div(1e12);
+        delegators[msg.sender].debtRewards = delegators[msg.sender]
+            .tsteemAmount
+            .mul(shareAcc)
+            .div(1e12);
 
         emit DepositTSTEEM(msg.sender, _amount);
     }
 
-    function withdraw(uint256 _amount) 
-        public
-        onlyDelegator
-    {
+    function withdraw(uint256 _amount) public onlyDelegator {
         if (_amount == 0) return;
 
         if (delegators[msg.sender].tsteemAmount == 0) return;
 
         _updateRewardInfo();
 
-        uint256 pending = delegators[msg.sender].tsteemAmount.mul(shareAcc).div(1e12).sub(delegators[msg.sender].debtRewards);
-        if(pending > 0) {
-            delegators[msg.sender].availablePeanuts = delegators[msg.sender].availablePeanuts.add(pending);
+        uint256 pending =
+            delegators[msg.sender].tsteemAmount.mul(shareAcc).div(1e12).sub(
+                delegators[msg.sender].debtRewards
+            );
+        if (pending > 0) {
+            delegators[msg.sender].availablePeanuts = delegators[msg.sender]
+                .availablePeanuts
+                .add(pending);
         }
-        
+
         uint256 withdrawAmount;
         if (_amount >= delegators[msg.sender].tsteemAmount)
             withdrawAmount = delegators[msg.sender].tsteemAmount;
-        else
-            withdrawAmount = _amount;
+        else withdrawAmount = _amount;
 
         // transfer TSTEEM from this to delegator
         Tsteem.transfer(msg.sender, withdrawAmount);
-        delegators[msg.sender].tsteemAmount = delegators[msg.sender].tsteemAmount.sub(withdrawAmount);
+        delegators[msg.sender].tsteemAmount = delegators[msg.sender]
+            .tsteemAmount
+            .sub(withdrawAmount);
         totalDepositedTSTEEM = totalDepositedTSTEEM.sub(withdrawAmount);
 
         // now game stopped, reset contract status
-        if (totalDepositedTSTEEM == 0)
-            _resetStatus();
+        if (totalDepositedTSTEEM == 0) _resetStatus();
 
-        delegators[msg.sender].debtRewards = delegators[msg.sender].tsteemAmount.mul(shareAcc).div(1e12);
+        delegators[msg.sender].debtRewards = delegators[msg.sender]
+            .tsteemAmount
+            .mul(shareAcc)
+            .div(1e12);
 
+        // auto withdraw peanuts when he undeposite
+        if (delegators[msg.sender].tsteemAmount == 0) {
+            Pnuts.transfer(msg.sender, delegators[msg.sender].availablePeanuts);
+            delegators[msg.sender].availablePeanuts = 0;
+            emit WithdrawPeanuts(
+                msg.sender,
+                delegators[msg.sender].availablePeanuts
+            );
+        }
         emit WithdrawTSTEEM(msg.sender, withdrawAmount);
     }
-    
-    function withdrawPeanuts() 
-        public
-        onlyDelegator
-    {
+
+    function withdrawPeanuts() public onlyDelegator {
         // game has not started
         if (lastRewardBlock == 0) return;
 
         // There are new blocks created after last updating, so append new rewards before withdraw
-        if(block.number > lastRewardBlock) {
+        if (block.number > lastRewardBlock) {
             _updateRewardInfo();
         }
 
-        uint256 pending = delegators[msg.sender].tsteemAmount.mul(shareAcc).div(1e12).sub(delegators[msg.sender].debtRewards);
-        if(pending > 0) {
-            delegators[msg.sender].availablePeanuts = delegators[msg.sender].availablePeanuts.add(pending);
+        uint256 pending =
+            delegators[msg.sender].tsteemAmount.mul(shareAcc).div(1e12).sub(
+                delegators[msg.sender].debtRewards
+            );
+        if (pending > 0) {
+            delegators[msg.sender].availablePeanuts = delegators[msg.sender]
+                .availablePeanuts
+                .add(pending);
         }
 
         Pnuts.transfer(msg.sender, delegators[msg.sender].availablePeanuts);
 
-        delegators[msg.sender].debtRewards = delegators[msg.sender].tsteemAmount.mul(shareAcc).div(1e12);
-
-        emit WithdrawPeanuts(msg.sender, delegators[msg.sender].availablePeanuts);
-
+        delegators[msg.sender].debtRewards = delegators[msg.sender]
+            .tsteemAmount
+            .mul(shareAcc)
+            .div(1e12);
         delegators[msg.sender].availablePeanuts = 0;
+
+        emit WithdrawPeanuts(
+            msg.sender,
+            delegators[msg.sender].availablePeanuts
+        );
     }
-    
+
     // pending peanuts >= delegator.availablePeanuts
     function getPendingPeanuts() public view returns (uint256) {
         // game has not started
@@ -180,16 +223,27 @@ contract TsteemPool is Ownable {
         // the right amount that delegator can award
         if (currentBlock > lastRewardBlock) {
             uint256 _shareAcc = shareAcc;
-            uint256 unmintedPeanuts = _calculateRewardToDelegators();
-            _shareAcc = _shareAcc.add(unmintedPeanuts.div(1e12).mul(1e12).div(totalDepositedTSTEEM));
-            uint256 pending = delegators[msg.sender].tsteemAmount.mul(_shareAcc).div(1e12).sub(delegators[msg.sender].debtRewards);
+            uint256 totalPendingPnuts = _getTotalPendingPnut();
+            uint256 unmintedPeanuts =
+                totalPendingPnuts.mul(9e11).mul(totalDepositedTSTEEM).div(
+                    Tsteem.totalSupply()
+                );
+            _shareAcc = _shareAcc.add(
+                unmintedPeanuts.div(1e12).mul(1e12).div(totalDepositedTSTEEM)
+            );
+            uint256 pending =
+                delegators[msg.sender]
+                    .tsteemAmount
+                    .mul(_shareAcc)
+                    .div(1e12)
+                    .sub(delegators[msg.sender].debtRewards);
             return delegators[msg.sender].availablePeanuts.add(pending);
         } else {
             return delegators[msg.sender].availablePeanuts;
         }
     }
 
-    function getDelegatorListLength() public view returns(uint256) {
+    function getDelegatorListLength() public view returns (uint256) {
         return delegatorsList.length;
     }
 
@@ -198,7 +252,6 @@ contract TsteemPool is Ownable {
     }
 
     function _updateRewardInfo() internal {
-
         // game has not started
         if (lastRewardBlock == 0) return;
 
@@ -207,13 +260,17 @@ contract TsteemPool is Ownable {
         uint256 currentBlock = block.number;
 
         // make sure one block can only be calculated one time.
-        // think about this situation that more than one deposit/withdraw/withdrowPeanuts transactions 
-        // were exist in the same block, delegator.amout should be updated after _updateRewardInfo being 
+        // think about this situation that more than one deposit/withdraw/withdrowPeanuts transactions
+        // were exist in the same block, delegator.amout should be updated after _updateRewardInfo being
         // invoked and it's award peanuts should be calculated next time
         if (currentBlock <= lastRewardBlock) return;
-
-        uint256 peanutsMintedToDev = _calculateRewardToDev();
-        uint256 peanutsMintedToDelegators = _calculateRewardToDelegators();
+        uint256 totalPendingPnuts = _getTotalPendingPnut();
+        uint256 peanutsMintedToDelegators =
+            totalPendingPnuts.mul(9e11).mul(totalDepositedTSTEEM).div(
+                Tsteem.totalSupply()
+            );
+        uint256 peanutsMintedToDev =
+            totalPendingPnuts.mul(1e12).sub(peanutsMintedToDelegators);
 
         // rewards belong to delegators temporary saved in contract, need delegator withdraw it
         PnutPool.withdrawPeanuts();
@@ -225,20 +282,17 @@ contract TsteemPool is Ownable {
         if (totalDepositedTSTEEM == 0) {
             _resetStatus();
         } else {
-            shareAcc = shareAcc.add(peanutsMintedToDelegators.div(1e12).mul(1e12).div(totalDepositedTSTEEM));
+            shareAcc = shareAcc.add(
+                peanutsMintedToDelegators.div(1e12).mul(1e12).div(
+                    totalDepositedTSTEEM
+                )
+            );
         }
 
         lastRewardBlock = block.number;
     }
 
-    // return  (amount of peanuts)*1e12
-    function _calculateRewardToDelegators() internal view returns (uint256) {
-        return PnutPool.getPendingPeanuts().mul(totalDepositedTSTEEM.mul(9e11).div(Tsteem.totalSupply()));
-        return PnutPool.getPendingPeanuts().mul(9e11);
-    }
-
-    // return (amount of peanuts)*1e12
-    function _calculateRewardToDev() internal view returns (uint256) {
-        return PnutPool.getPendingPeanuts().mul(1e11);
+    function _getTotalPendingPnut() internal view returns (uint256) {
+        return PnutPool.getPendingPeanuts();
     }
 }
